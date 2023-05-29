375c1744f9f2a306b617e22ad8f4a0aaf60d3539
bharatc12 <98902628+bharatc12@users.noreply.github.com>
2023-05-29 15:16:06 -0400

    Update ingest script.py

--- ingest script.py
+++ ingest script.py
@@ -16,39 +16,24 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
-# Regular expressions for log line parsing
-apache_log_regex = re.compile(r'^(\S+)\s+\S+\s+(\S+)\s+\[(.*?)\]\s+"(\S+)\s+(\S+)\s+HTTP/\d\.\d"\s+(\d+)\s+(\d+)\s+"([^"]+)"')
-exim4_log_regex = re.compile(r'^(\S+)\s+\S+\s+(\S+)\s+<([^>]+)>\s+.*\s+id=.*\s+(.*):\s+(.*);\s+(.*)')
+# Regular expression for log line parsing
+log_regex = re.compile(r'^(\S+)\s+-\s+-\s+\[(.*?)\]\s+"(\S+)\s+(\S+)\s+HTTP/\d\.\d"\s+(\d+)\s+(\d+|-)"')
 
-# Log file paths
-apache_log_file = 'path_to_apache_log_file'
-exim4_log_file = 'path_to_exim4_log_file'
+# Log file path
+log_file = 'path_to_log_file'
 
-# Function to insert Apache log entries into the database
-def insert_apache_log_entry(log_entry):
-    query = "INSERT INTO apache_logs (remote_host, timestamp, request_method, requested_url, http_status_code, bytes_transferred, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+# Function to insert log entries into the database
+def insert_log_entry(log_entry):
+    query = "INSERT INTO apache_logs (remote_host, timestamp, request_method, requested_url, http_status_code, bytes_transferred) VALUES (%s, %s, %s, %s, %s, %s)"
     cursor.execute(query, log_entry)
 
-# Function to insert Exim4 log entries into the database
-def insert_exim4_log_entry(log_entry):
-    query = "INSERT INTO exim4_logs (timestamp, sender, recipient, message, status, error_message) VALUES (%s, %s, %s, %s, %s, %s)"
-    cursor.execute(query, log_entry)
-
-# Read and parse Apache log file
-with open(apache_log_file, 'r') as file:
+# Read and parse log file
+with open(log_file, 'r') as file:
     for line in file:
-        match = apache_log_regex.match(line)
+        match = log_regex.match(line)
         if match:
             log_entry = match.groups()
-            insert_apache_log_entry(log_entry)
-
-# Read and parse Exim4 log file
-with open(exim4_log_file, 'r') as file:
-    for line in file:
-        match = exim4_log_regex.match(line)
-        if match:
-            log_entry = match.groups()
-            insert_exim4_log_entry(log_entry)
+            insert_log_entry(log_entry)
 
 # Commit the changes and close the database connection
 conn.commit()
