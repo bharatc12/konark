38a5c070e869cd6d5bb835389dbb2a681a186559
bharatc12 <98902628+bharatc12@users.noreply.github.com>
2023-06-09 13:18:37 -0400

    Add files via upload

---
+++ emailstoragescript.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import psycopg2
+
+# Connect to the PostgreSQL database
+db = psycopg2.connect(
+    host="localhost",
+    user="postgres",
+    password="t@rHB123",
+    database="emails"
+)
+
+# Obtain the necessary information from the command line arguments
+mail_from = sys.argv[1]
+rcpt_to = sys.argv[2]
+body = sys.stdin.read()
+
+# Insert the email into the database
+cursor = db.cursor()
+query = "INSERT INTO emails (sender, recipient, body) VALUES (%s, %s, %s)"
+values = (mail_from, rcpt_to, body)
+cursor.execute(query, values)
+db.commit()
+
+# Close the database connection
+db.close()
