e403603145c5a8833532f562e898b2c5f7c0c311
bharatc12 <98902628+bharatc12@users.noreply.github.com>
2023-06-09 13:42:08 -0400

    Update emailstoragescript.py

--- emailstoragescript.py
+++ emailstoragescript.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+#built in module to python
 import sys
+#module used to connect to postgres
 import psycopg2
 
-Connect to the PostgreSQL database
+#Connect to the PostgreSQL database
 db = psycopg2.connect(
 host="localhost",
 user="your_username",
@@ -11,12 +13,12 @@ password="your_password",
 database="your_database"
 )
 
-Obtain the necessary information from the script arguments
+#Obtain the necessary information from the script arguments
 mail_from = sys.argv[1]
 rcpt_to = sys.argv[2]
 body = sys.stdin.read()
 
-Check if the email is a relay email
+#Check if the email is a relay email
 is_relay = False
 if "@" in mail_from and "@" in rcpt_to:
 mail_from_domain = mail_from.split("@")[1]
@@ -24,7 +26,7 @@ rcpt_to_domain = rcpt_to.split("@")[1]
 if mail_from_domain == rcpt_to_domain:
 is_relay = True
 
-Insert the email into the database if it is not a relay email
+#Insert the email into the database if it is not a relay email
 if not is_relay:
 cursor = db.cursor()
 query = "INSERT INTO email_logs (mail_from, rcpt_to, body) VALUES (%s, %s, %s)"
@@ -32,5 +34,5 @@ values = (mail_from, rcpt_to, body)
 cursor.execute(query, values)
 db.commit()
 
-Close the database connection
+#Close the database connection
 db.close()
